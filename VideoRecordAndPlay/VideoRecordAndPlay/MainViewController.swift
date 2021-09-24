e03d679ba82ba6c96ecd830574fb503bf510165e
YounByoungil <jimmy7525@naver.com>
2021-09-24 15:45:32 +0900

    Setup Project

---
+++ VideoRecordAndPlay/VideoRecordAndPlay/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  VideoRecordAndPlay
+//
+//  Created by 윤병일 on 2021/09/24.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .red
+  }
+  
+  //MARK: - @objc func
+  
+}
