d6f7b221a6284932c2fca13e04f901062ea89262
YounByoungil <jimmy7525@naver.com>
2021-10-26 17:02:34 +0900

    영상 찍기 기능까지는 최종 구현 완료

--- VideoRecordAndTrimPractice/VideoRecordAndTrimPractice/RecordingViewController.swift
+++ VideoRecordAndTrimPractice/VideoRecordAndTrimPractice/RecordingViewController.swift
@@ -13,23 +13,21 @@ import CoreMotion
 import Then
 import SnapKit
 
-class RecordingViewController : UIViewController {
+class RecordingViewController: UIViewController {
   
-  //MARK: - Properties
+  // MARK:- Properties
   
-  private let disposeBag = DisposeBag()
+  var disposeBag = DisposeBag()
   
-  let captureSession = AVCaptureSession().then {
-    $0.sessionPreset = .hd1280x720 // 녹화 품질을 설정하는 속성
-  }
+  let captureSession = AVCaptureSession()
   
-  var videoDevice : AVCaptureDevice!
+  var videoDevice: AVCaptureDevice!
   
-  var videoInput : AVCaptureDeviceInput!
+  var videoInput: AVCaptureDeviceInput!
   
-  var audioInput : AVCaptureDeviceInput!
+  var audioInput: AVCaptureDeviceInput!
   
-  var videoOutput : AVCaptureMovieFileOutput!
+  var videoOutput: AVCaptureMovieFileOutput!
   
   lazy var previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession).then {
     $0.bounds = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
@@ -38,15 +36,8 @@ class RecordingViewController : UIViewController {
   }
   
   let topContainer = UIView()
-  
-  let swapButton = UIButton().then {
-    $0.setTitle("화면교체", for: .normal)
-  }
-  
-  let recordButton = UIButton().then {
-    $0.backgroundColor = .lightGray
-  }
-  
+  let swapButton = UIButton().then { $0.setTitle("Swap", for: .normal) }
+  let recordButton = UIButton().then { $0.setTitle("Record", for: .normal) }
   let recordPoint = UIView().then {
     $0.backgroundColor = UIColor(red: 1.0, green: 0.75, blue: 0.01, alpha: 1)
     $0.layer.cornerRadius = 3
@@ -58,21 +49,15 @@ class RecordingViewController : UIViewController {
     $0.textColor = .white
   }
   
-  var outputURL : URL?
-  var motionManager : CMMotionManager!
-  var deviceOrientation : AVCaptureVideoOrientation = .landscapeLeft
+  var outputURL: URL?
+  var motionManager: CMMotionManager!
+  var deviceOrientation: AVCaptureVideoOrientation = .portrait
   
-  var timer : Timer?
+  var timer: Timer?
   var secondsOfTimer = 0
   
-  //MARK: - Lifecycle
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    configureUI()
-    bind()
-    videoDevice = bestDevice(in: .front)
-    setupSession()
-  }
+  
+  // MARK:- LifeCycle Methods
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
@@ -89,27 +74,19 @@ class RecordingViewController : UIViewController {
     stopTimer()
   }
   
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-    configureVideoOrientation()
-  }
-  
-  //MARK: - Functions
-  private func configureVideoOrientation() {
-      if  previewLayer == self.previewLayer,
-          let connection = previewLayer.connection {
-          let orientation = UIDevice.current.orientation
-
-          if connection.isVideoOrientationSupported,
-              let videoOrientation = AVCaptureVideoOrientation(rawValue: orientation.rawValue) {
-              previewLayer.frame = self.view.bounds
-              connection.videoOrientation = videoOrientation
-          }
-      }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    layout()
+    bind()
+    videoDevice = bestDevice(in: .back)
+    setupSession()
   }
   
   
-  private func configureUI() {
+  // MARK:- View Rendering
+  
+  private func layout() {
     self.view.layer.addSublayer(previewLayer)
     
     self.view.addSubview(topContainer)
@@ -142,59 +119,35 @@ class RecordingViewController : UIViewController {
     recordButton.snp.makeConstraints {
       $0.centerX.equalToSuperview()
       $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-50)
-      $0.width.height.equalTo(40)
+      $0.height.equalTo(40)
     }
-    
-    recordButton.layer.cornerRadius = 40 / 2
   }
+  
+  
+  // MARK:- Rx Binding
   
   private func bind() {
     recordButton.rx.tap
-      .bind { [weak self] in
-        guard let self = self else {return}
+      .subscribe(onNext: { [weak self] in
+        guard let `self` = self else { return }
         
         if self.videoOutput.isRecording {
           self.stopRecording()
-          self.recordButton.backgroundColor = .lightGray
+          self.recordButton.setTitle("Record", for: .normal)
         } else {
           self.startRecording()
-          self.recordButton.backgroundColor = .red
+          self.recordButton.setTitle("Stop", for: .normal)
         }
-      }.disposed(by: self.disposeBag)
+      })
+      .disposed(by: self.disposeBag)
     
     swapButton.rx.tap
       .subscribe(onNext: { [weak self] in
-        guard let self = self else { return }
+        guard let `self` = self else { return }
         self.swapCameraType()
       })
       .disposed(by: self.disposeBag)
   }
-  
-  // 가속도계(자이로스코프)를 측정해서 화면이 Lock 상태에서도 orientation 구하기.
-  private func initMotionManager() {
-    motionManager = CMMotionManager()
-    motionManager.accelerometerUpdateInterval = 0.2
-    motionManager.gyroUpdateInterval = 0.2
-    
-    motionManager.startAccelerometerUpdates( to: OperationQueue() ) { [weak self] accelerometerData, _ in
-      guard let data = accelerometerData else { return }
-      
-      if abs(data.acceleration.y) < abs(data.acceleration.x) {
-        if data.acceleration.x > 0 {
-          self?.deviceOrientation = .landscapeLeft
-        } else {
-          self?.deviceOrientation = .landscapeRight
-        }
-      } else {
-        if data.acceleration.y > 0 {
-          self?.deviceOrientation = .portraitUpsideDown
-        } else {
-          self?.deviceOrientation = .portrait
-        }
-      }
-    }
-  }
-  
   
   private func setupSession() {
     do {
@@ -274,26 +227,28 @@ class RecordingViewController : UIViewController {
   }
   
   
+  // MARK:- Recording Methods
+  
   private func startRecording() {
     let connection = videoOutput.connection(with: AVMediaType.video)
     
+    // orientation을 설정해야 가로/세로 방향에 따른 레코딩 출력이 잘 나옴.
     if (connection?.isVideoOrientationSupported)! {
       connection?.videoOrientation = self.deviceOrientation
     }
     
     let device = videoInput.device
-    
     if (device.isSmoothAutoFocusSupported) {
       do {
         try device.lockForConfiguration()
         device.isSmoothAutoFocusEnabled = false
         device.unlockForConfiguration()
       } catch {
-        print("Error setting configuration \(error)")
+        print("Error setting configuration: \(error)")
       }
     }
     
-    // recording point, timerString 에 대한 핸들링
+    // recording point, timerString에 대한 핸들링
     recordPoint.alpha = 1
     self.fadeViewInThenOut(view: recordPoint, delay: 0)
     self.startTimer()
@@ -310,6 +265,14 @@ class RecordingViewController : UIViewController {
     }
   }
   
+  private func fadeViewInThenOut(view : UIView, delay: TimeInterval) {
+    let animationDuration = 0.5
+    
+    UIView.animate(withDuration: animationDuration, delay: delay, options: [UIView.AnimationOptions.autoreverse, UIView.AnimationOptions.repeat], animations: {
+      view.alpha = 0
+    }, completion: nil)
+  }
+  
   private func tempURL() -> URL? {
     let directory = NSTemporaryDirectory() as NSString
     
@@ -321,21 +284,61 @@ class RecordingViewController : UIViewController {
     return nil
   }
   
-  private func fadeViewInThenOut(view : UIView, delay: TimeInterval) {
-    let animationDuration = 0.5
+  // 가속도계(자이로스코프)를 측정해서 화면이 Lock 상태에서도 orientation 구하기.
+  private func initMotionManager() {
+    motionManager = CMMotionManager()
+    motionManager.accelerometerUpdateInterval = 0.2
+    motionManager.gyroUpdateInterval = 0.2
     
-    UIView.animate(withDuration: animationDuration, delay: delay, options: [UIView.AnimationOptions.autoreverse, UIView.AnimationOptions.repeat], animations: {
-      view.alpha = 0
-    }, completion: nil)
+    motionManager.startAccelerometerUpdates( to: OperationQueue() ) { [weak self] accelerometerData, _ in
+      guard let data = accelerometerData else { return }
+      
+      if abs(data.acceleration.y) < abs(data.acceleration.x) {
+        if data.acceleration.x > 0 {
+          self?.deviceOrientation = .landscapeLeft
+        } else {
+          self?.deviceOrientation = .landscapeRight
+        }
+      } else {
+        if data.acceleration.y > 0 {
+          self?.deviceOrientation = .portraitUpsideDown
+        } else {
+          self?.deviceOrientation = .portrait
+        }
+      }
+    }
   }
+  
+  public func imageFromVideo(url: URL, at time: TimeInterval, completion: @escaping (UIImage?) -> Void) {
+      DispatchQueue.global(qos: .background).async {
+          let asset = AVURLAsset(url: url)
 
+          let assetIG = AVAssetImageGenerator(asset: asset)
+          assetIG.appliesPreferredTrackTransform = true
+          assetIG.apertureMode = AVAssetImageGenerator.ApertureMode.encodedPixels
+
+          let cmTime = CMTime(seconds: time, preferredTimescale: 60)
+        
+          let thumbnailImageRef: CGImage
+          do {
+              thumbnailImageRef = try assetIG.copyCGImage(at: cmTime, actualTime: nil)
+          } catch let error {
+              print("Error: \(error)")
+              return completion(nil)
+          }
+
+          DispatchQueue.main.async {
+              completion(UIImage(cgImage: thumbnailImageRef))
+          }
+      }
+  }
   
   
   // MARK:- Timer methods
   
   private func startTimer() {
     timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
-      guard let self = self else { return }
+      guard let `self` = self else { return }
       
       self.secondsOfTimer += 1
       self.timerLabel.text = Double(self.secondsOfTimer).format(units: [.hour ,.minute, .second])
@@ -347,28 +350,26 @@ class RecordingViewController : UIViewController {
     self.timerLabel.text = "00:00:00"
   }
   
-  //MARK: - @objc func
-  
 }
 
-  //MARK: - AVCaptureFileOutputRecordingDelegat
-extension RecordingViewController : AVCaptureFileOutputRecordingDelegate {
+
+extension RecordingViewController: AVCaptureFileOutputRecordingDelegate {
   
+  // 레코딩이 시작되면 호출
   func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
-    // 레코딩이 시작되면 호출
+    
   }
   
+  // 레코딩이 끝나면 호출
   func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
-    // 레코딩이 끝나면 호출
     if (error != nil) {
-      print("Error recording movie : \(error!.localizedDescription)")
+      print("Error recording movie: \(error!.localizedDescription)")
     } else {
       let videoRecorded = outputURL! as URL
       UISaveVideoAtPathToSavedPhotosAlbum(videoRecorded.path, nil, nil, nil)
     }
   }
 }
-
 
 extension Double {
   func format(units: NSCalendar.Unit) -> String {
@@ -380,4 +381,3 @@ extension Double {
     return formatter.string(from: self)!
   }
 }
-
